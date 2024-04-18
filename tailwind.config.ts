b04a4c8517a940c52f496cb9d7a68e7bc5aed37d
Santiago Casanova <tyagocasanova@gmail.com>
2024-04-19 00:13:52 +0200

    chore: adding new icons library

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,14 @@ export default {
       borderRadius: {
         lg: "30px",
       },
+      colors: {
+        dark: {
+          950: "#0f0f0f",
+          900: "rgb(18, 18, 18)",
+          850: "rgb(48, 48, 48)",
+          800: "rgba(255, 255, 255, 0.08)",
+        },
+      },
     },
   },
   plugins: [],
