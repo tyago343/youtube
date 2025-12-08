84b13a65233cd4992135ff9927a8348257473108
Santiago Casanova <tyagocasanova@gmail.com>
2025-12-08 20:07:08 +0100

    chore: changing favicon and adding home

--- frontend/src/routes/public.routes.tsx
+++ frontend/src/routes/public.routes.tsx
@@ -1,8 +1,9 @@
 import Login from "../features/auth/pages/Login";
 import Signup from "../features/auth/pages/Signup";
 import { Route } from "react-router";
-
+import Home from "@pages/Home";
 export const publicRoutes = [
   <Route path="/login" element={<Login />} />,
   <Route path="/signup" element={<Signup />} />,
+  <Route path="/" element={<Home />} />,
 ];
