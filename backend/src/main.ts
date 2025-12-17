335c0c4fa0fef7a8df82cc62b34c688cdd80b11b
Santiago Casanova <tyagocasanova@gmail.com>
2025-12-17 14:21:49 +0100

    chore: adding swagger to project

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
+  const config = new DocumentBuilder()
+    .setTitle('OpenTube API')
+    .setDescription('OpenTube API documentation')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1', app, documentFactory);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
