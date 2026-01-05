f6a64aeafa077d0aa7568d9696d39bd9de0780b9
Santiago Casanova <tyagocasanova@gmail.com>
2026-01-06 00:56:59 +0100

    chore: adding button to create video

--- frontend/src/modules/user/components/user-menu.component.tsx
+++ frontend/src/modules/user/components/user-menu.component.tsx
@@ -10,6 +10,8 @@ import {
 } from "@shared/ui/navigation-menu/navigation-menu";
 import { useDispatch } from "react-redux";
 import { logout } from "@auth/model/auth.actions";
+import Button from "@shared/ui/button/button";
+import { PlusIcon } from "lucide-react";
 
 function UserMenuComponent({ user }: { user: User }) {
   const navigate = useNavigate();
@@ -19,27 +21,30 @@ function UserMenuComponent({ user }: { user: User }) {
     navigate("/");
   }
   return (
-    <NavigationMenu viewport={false}>
-      <NavigationMenuItem className="hidden md:block bg-none border-none">
-        <NavigationMenuTrigger className="bg-none border-none outline-none">
-          <Avatar className="cursor-pointer">
-            <AvatarImage src={user.avatarUrl} alt={user.username} />
-            <AvatarFallback>{user.username.charAt(0)}</AvatarFallback>
-          </Avatar>
-        </NavigationMenuTrigger>
-        <NavigationMenuContent className="min-w-[180px] left-auto right-0 z-50">
-          <NavigationMenuLink>
-            <Link to="/profile">Profile</Link>
-          </NavigationMenuLink>
-          <NavigationMenuLink>
-            <p onClick={onLogout}>Logout</p>
-          </NavigationMenuLink>
-          <NavigationMenuLink>
-            <Link to="/upload-video">Upload Video</Link>
-          </NavigationMenuLink>
-        </NavigationMenuContent>
-      </NavigationMenuItem>
-    </NavigationMenu>
+    <div className="flex items-center gap-2">
+      <Button variant="secondary" className="rounded-full">
+        <PlusIcon className="size-4" />
+        <Link to="/upload-video">Create</Link>
+      </Button>
+      <NavigationMenu viewport={false}>
+        <NavigationMenuItem className="hidden md:block bg-none border-none">
+          <NavigationMenuTrigger className="bg-none border-none outline-none">
+            <Avatar className="cursor-pointer">
+              <AvatarImage src={user.avatarUrl} alt={user.username} />
+              <AvatarFallback>{user.username.charAt(0)}</AvatarFallback>
+            </Avatar>
+          </NavigationMenuTrigger>
+          <NavigationMenuContent className="min-w-[180px] left-auto right-0 z-50">
+            <NavigationMenuLink>
+              <Link to="/profile">Profile</Link>
+            </NavigationMenuLink>
+            <NavigationMenuLink>
+              <p onClick={onLogout}>Logout</p>
+            </NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenu>
+    </div>
   );
 }
 export default UserMenuComponent;
