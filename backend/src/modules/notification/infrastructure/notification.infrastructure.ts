f2439ddca836efe74bedce1c91437f1fa40e0f2d
Santiago Casanova <tyagocasanova@gmail.com>
2026-02-04 18:00:09 +0100

    refactor: remove unused report repository imports and integrate ReportsInfrastructureModule into reports module

--- backend/src/modules/notification/infrastructure/notification.infrastructure.ts
+++ backend/src/modules/notification/infrastructure/notification.infrastructure.ts
@@ -4,10 +4,6 @@ import { EmailSender } from '../application/ports/email-sender.interface';
 import { TemplateRenderer } from '../application/ports/template-renderer.interface';
 import { ResendEmailService } from './email/resend-email.service';
 import { HandlebarsRendererService } from './templates/handlebars-renderer.service';
-import { ReportsRepository } from 'src/modules/reports/application/ports/reports.repository';
-import { OrmReportRepository } from 'src/modules/reports/infrastructure/persistence/typeorm/repositories/orm-report.repository';
-import { ReportWatcherRepository } from 'src/modules/reports/application/ports/report-watcher.repository';
-import { OrmReportWatcherRepository } from 'src/modules/reports/infrastructure/persistence/typeorm/repositories/orm-report-watcher.repository';
 
 @Module({
   imports: [ConfigModule],
@@ -19,14 +15,6 @@ import { OrmReportWatcherRepository } from 'src/modules/reports/infrastructure/p
     {
       provide: TemplateRenderer,
       useClass: HandlebarsRendererService,
-    },
-    {
-      provide: ReportsRepository,
-      useClass: OrmReportRepository,
-    },
-    {
-      provide: ReportWatcherRepository,
-      useClass: OrmReportWatcherRepository,
     },
   ],
   exports: [EmailSender, TemplateRenderer],
--- backend/src/modules/reports/reports.module.ts
+++ backend/src/modules/reports/reports.module.ts
@@ -3,9 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReportSchema } from './infrastructure/persistence/typeorm/entities/report.schema';
 import { ReportWatcherSchema } from './infrastructure/persistence/typeorm/entities/report-watcher.schema';
 import { ReportsPresentersModule } from './presenters/reports.presenters';
+import { ReportsInfrastructureModule } from './infrastructure/reports.infrestructure';
+
 @Module({
   imports: [
     TypeOrmModule.forFeature([ReportSchema, ReportWatcherSchema]),
+    ReportsInfrastructureModule,
     ReportsPresentersModule,
   ],
 })
